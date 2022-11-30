b51e9968bec8c2252514607148f39f18c227aece
CakraDarma <baguscakraa02@gmail.com>
2022-11-30 14:22:53 +0800

    create admin layout

--- components/common/AdminNav.tsx
+++ components/common/AdminNav.tsx
@@ -55,22 +55,18 @@ const AdminNav: FC<Props> = ({ navItems }): JSX.Element => {
 		<nav ref={navRef} className='h-screen w-60 shadow-sm bg-secondary-light dark:bg-secondary-dark flex flex-col justify-between transition-width overflow-hidden sticky top-0'>
 			<div>
 				{/* logo */}
-				<Link href='/admin'>
-					{/* <a className="flex items-center space-x-2 p-3 mb-10"> */}
+				<Link href='/admin' className='flex items-center space-x-2 p-3 mb-10'>
 					<Logo className='fill-highlight-light dark:fill-highlight-dark w-5 h-5' />
 					{visible && <span className='text-highlight-light dark:text-highlight-dark text-xl font-semibold leading-none'>Admin</span>}
-					{/* </a> */}
 				</Link>
 
 				{/* nav items */}
 				<div className='space-y-6'>
 					{navItems.map((item) => {
 						return (
-							<Link key={item.href} href={item.href}>
-								{/* <a className="flex items-center text-highlight-light dark:text-highlight-dark text-xl p-3 hover:scale-[0.98] transition"> */}
+							<Link key={item.href} href={item.href} className='flex items-center text-highlight-light dark:text-highlight-dark text-xl p-3 hover:scale-[0.98] transition'>
 								<item.icon size={24} />
 								{visible && <span className='ml-2 leading-none'>{item.label}</span>}
-								{/* </a> */}
 							</Link>
 						);
 					})}
--- components/layout/AdminLayout.tsx
+++ components/layout/AdminLayout.tsx
@@ -21,11 +21,8 @@ const AdminLayout: FC<Props> = ({ children }): JSX.Element => {
 		<div className='flex '>
 			<AdminNav navItems={navItems} />
 			<div className='flex-1 p-4'>{children}</div>
-			{/* create button */}
-			<Link href='/admin/post/create'>
-				{/* <a className="bg-secondary-dark dark:bg-secondary-light text-primary dark:text-primary-dark fixed z-10 right-10 bottom-10 p-3 rounded-full hover:scale-90 shadow-sm transition"> */}
+			<Link href='/admin/post/create' className='bg-secondary-dark dark:bg-secondary-light text-primary dark:text-primary-dark fixed z-10 right-10 bottom-10 p-3 rounded-full hover:scale-90 shadow-sm transition'>
 				<AiOutlineFileAdd size={24} />
-				{/* </a> */}
 			</Link>
 		</div>
 	);
